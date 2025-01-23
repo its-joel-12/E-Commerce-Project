706a77898303dba73becd784f7b93c07ec80428e
Joel Oliveira <joel80851@gmail.com>
2025-01-23 12:42:34 +0530

    @RequestMapping Annotation

--- src/main/java/com/joel/controller/CategoryController.java
+++ src/main/java/com/joel/controller/CategoryController.java
@@ -12,24 +12,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("api")
 public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
 
-    @GetMapping("api/public/categories")
+    @GetMapping("public/categories")
     public ResponseEntity<List<Category>> getAllCategories() {
         return new ResponseEntity<>(categoryService.getAllCategories(), HttpStatus.OK);
     }
 
-    @PostMapping("api/public/categories")
+    @PostMapping("public/categories")
     public ResponseEntity<String> createCategory(@RequestBody Category category) {
         categoryService.createCategory(category);
         return new ResponseEntity<>("Category Added Successfully", HttpStatus.CREATED);
 
     }
 
-    @DeleteMapping("api/admin/categories/{categoryId}")
+    @DeleteMapping("admin/categories/{categoryId}")
     public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId) {
         try {
             String status = categoryService.deleteCategory(categoryId);
@@ -39,7 +40,7 @@ public class CategoryController {
         }
     }
 
-    @PutMapping("api/admin/categories/{categoryId}")
+    @PutMapping("admin/categories/{categoryId}")
     public ResponseEntity<String> updateCategory(@RequestBody Category category, @PathVariable Long categoryId) {
         try {
             Category updateCategory = categoryService.updateCategory(category, categoryId);
