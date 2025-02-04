c885d65162f4a54146b34729addb9078c86a5c61
Joel Oliveira <joel80851@gmail.com>
2025-02-04 11:57:16 +0530

    Implementing ResourceNotFoundException

---
+++ src/main/java/com/joel/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.joel.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException() {
+        super("Resource not found on server !");
+    }
+}
