e3fb619e218f4e17a5c1f9e77671deaac12de56c
Joel Oliveira <joel80851@gmail.com>
2025-02-04 10:57:00 +0530

    Making use of @Valid

--- src/main/java/com/joel/controller/CategoryController.java
+++ src/main/java/com/joel/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.joel.controller;
 
 import com.joel.model.Category;
 import com.joel.service.CategoryService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,7 +25,7 @@ public class CategoryController {
     }
 
     @PostMapping("public/categories")
-    public ResponseEntity<String> createCategory(@RequestBody Category category) {
+    public ResponseEntity<String> createCategory(@Valid @RequestBody Category category) {
         categoryService.createCategory(category);
         return new ResponseEntity<>("Category Added Successfully", HttpStatus.CREATED);
 
