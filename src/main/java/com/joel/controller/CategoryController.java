23d214a4ed1b7a1deb78e2569eae5c0393c53664
Joel Oliveira <joel80851@gmail.com>
2025-02-04 15:42:36 +0530

    Cleaning Up Controllers

--- src/main/java/com/joel/controller/CategoryController.java
+++ src/main/java/com/joel/controller/CategoryController.java
@@ -7,9 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -33,21 +31,13 @@ public class CategoryController {
 
     @DeleteMapping("admin/categories/{categoryId}")
     public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId) {
-        try {
-            String status = categoryService.deleteCategory(categoryId);
-            return new ResponseEntity<>(status, HttpStatus.OK);
-        } catch (ResponseStatusException e) {
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
+        String status = categoryService.deleteCategory(categoryId);
+        return new ResponseEntity<>(status, HttpStatus.OK);
     }
 
     @PutMapping("admin/categories/{categoryId}")
-    public ResponseEntity<String> updateCategory(@RequestBody Category category, @PathVariable Long categoryId) {
-        try {
-            Category updateCategory = categoryService.updateCategory(category, categoryId);
-            return new ResponseEntity<>("Updated category with Id: " + categoryId, HttpStatus.OK);
-        } catch (ResponseStatusException e) {
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
+    public ResponseEntity<String> updateCategory(@Valid @RequestBody Category category, @PathVariable Long categoryId) {
+        categoryService.updateCategory(category, categoryId);
+        return new ResponseEntity<>("Updated category with Id: " + categoryId, HttpStatus.OK);
     }
 }
--- src/main/java/com/joel/service/CategoryServiceImpl.java
+++ src/main/java/com/joel/service/CategoryServiceImpl.java
@@ -5,19 +5,13 @@ import com.joel.exception.ResourceNotFoundException;
 import com.joel.model.Category;
 import com.joel.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
-
-    //    private List<Category> categories = new ArrayList<>();
-//    private Long nextId = 1L;
 
     @Autowired
     private CategoryRepository categoryRepository;
@@ -37,7 +31,6 @@ public class CategoryServiceImpl implements CategoryService {
         if(savedCategory != null){
             throw new ApiException("Category with name '" + category.getCategoryName() + "' already exists !");
         }
-//        category.setCategoryId(nextId++);
         categoryRepository.save(category);
     }
 
