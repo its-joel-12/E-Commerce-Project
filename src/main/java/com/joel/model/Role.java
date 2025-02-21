e76f5c6e04d13f4d5032f26b5d9762f70ce0825c
Joel Oliveira <joel80851@gmail.com>
2025-02-21 12:19:28 +0530

    Implement Relationships

--- src/main/java/com/joel/model/Role.java
+++ src/main/java/com/joel/model/Role.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @NoArgsConstructor
 @Getter
@@ -16,6 +19,13 @@ public class Role {
 
     @Enumerated(EnumType.STRING)
     private AppRole roleName;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
     public Role(AppRole roleName) {
         this.roleName = roleName;
