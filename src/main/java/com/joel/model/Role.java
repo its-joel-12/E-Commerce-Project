d31ab0603322d57d3ae49ed1d523e94b28da243a
Joel Oliveira <joel80851@gmail.com>
2025-02-21 12:12:31 +0530

    Implement Users

---
+++ src/main/java/com/joel/model/Role.java
@@ -0,0 +1,23 @@
+package com.joel.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer roleId;
+
+    @Enumerated(EnumType.STRING)
+    private AppRole roleName;
+
+    public Role(AppRole roleName) {
+        this.roleName = roleName;
+    }
+}
---
+++ src/main/java/com/joel/model/User.java
@@ -0,0 +1,43 @@
+package com.joel.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotBlank
+    @Size(max = 20)
+    private String userName;
+
+    @Email
+    @NotBlank
+    @Size(max = 50)
+    private String userEmail;
+
+    @NotBlank
+    @Size(max = 120)
+    private String userPassword;
+
+    public User(String userName, String userEmail, String userPassword) {
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+    }
+}
