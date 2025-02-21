922f74ef94036fbee3bae099ab5a44e31031099a
Joel Oliveira <joel80851@gmail.com>
2025-02-21 15:05:17 +0530

    uniqueConstraints attribute

--- src/main/java/com/joel/model/User.java
+++ src/main/java/com/joel/model/User.java
@@ -1,9 +1,6 @@
 package com.joel.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -17,6 +14,11 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@Table(name = "users",
+        uniqueConstraints = {
+        @UniqueConstraint(columnNames = "user_name"),
+        @UniqueConstraint(columnNames = "user_email")
+})
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
